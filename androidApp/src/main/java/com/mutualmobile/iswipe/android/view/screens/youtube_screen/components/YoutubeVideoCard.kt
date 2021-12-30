c07c7b0de16a561f9fcdae5a5e8599ce3955d910
Shubham Singh <shubham.singh@mutualmobile.com>
2021-12-31 00:35:40 +0530

    Added Popup Menu on YoutubeVideoCard

--- androidApp/src/main/java/com/mutualmobile/iswipe/android/view/screens/youtube_screen/components/YoutubeVideoCard.kt
+++ androidApp/src/main/java/com/mutualmobile/iswipe/android/view/screens/youtube_screen/components/YoutubeVideoCard.kt
@@ -1,9 +1,11 @@
 package com.mutualmobile.iswipe.android.view.screens.youtube_screen.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
@@ -11,6 +13,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Divider
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -37,6 +42,7 @@ fun YoutubeVideoCard(video: Item, youtubeViewModel: YoutubeViewModel = get(), ex
     val interactionSource by remember { mutableStateOf(MutableInteractionSource()) }
     val iconInteractionSource by remember { mutableStateOf(MutableInteractionSource()) }
     val coroutineScope = rememberCoroutineScope()
+    var isDropDownMenuExpanded by remember { mutableStateOf(false) }
     Column(
         modifier = Modifier.clickable(
             interactionSource = interactionSource,
@@ -84,16 +90,21 @@ fun YoutubeVideoCard(video: Item, youtubeViewModel: YoutubeViewModel = get(), ex
                 color = MaterialTheme.colorScheme.onSurface,
                 modifier = Modifier.fillMaxWidth(0.9f),
             )
-            Icon(
-                painter = painterResource(id = R.drawable.ic_more),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(18.dp)
-                    .clickable(
-                        interactionSource = iconInteractionSource,
-                        indication = rememberRipple(bounded = false)
-                    ) {}
-            )
+            Box {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_more),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(18.dp)
+                        .clickable(
+                            interactionSource = iconInteractionSource,
+                            indication = rememberRipple(bounded = false)
+                        ) {
+                            isDropDownMenuExpanded = true
+                        }
+                )
+                YoutubeVideoCardMoreDialog(isExpanded = isDropDownMenuExpanded, closeMenu = { isDropDownMenuExpanded = false })
+            }
         }
         Row(
             modifier = Modifier.padding(top = 4.dp, start = 12.dp, bottom = 16.dp),
@@ -104,6 +115,30 @@ fun YoutubeVideoCard(video: Item, youtubeViewModel: YoutubeViewModel = get(), ex
             YoutubeCardCaptionText(text = "${video.statistics?.viewCount} views")
             YoutubeCaptionDotDivider()
             YoutubeCardCaptionText(text = "${video.snippet?.publishedAt} ago")
+        }
+    }
+}
+
+@Composable
+fun YoutubeVideoCardMoreDialog(isExpanded: Boolean, closeMenu: () -> Unit) {
+    val listOfMenuItems = listOf(
+        "Save to Watch Later",
+        "Save to playlist",
+        "Download video",
+        "Share",
+        "Not interested",
+        "Don't recommend channel",
+        "Report"
+    )
+    DropdownMenu(
+        expanded = isExpanded,
+        onDismissRequest = { closeMenu() },
+        modifier = Modifier.background(MaterialTheme.colorScheme.surface)
+    ) {
+        listOfMenuItems.forEach { menuItemText ->
+            DropdownMenuItem(onClick = { closeMenu() }) {
+                Text(menuItemText)
+            }
         }
     }
 }
