58da35d721137e67486a48bb3c348d6747cb6ca5
Shubham Singh <shubham.singh@mutualmobile.com>
2021-12-30 21:19:25 +0530

    Added tags in ExpandedPlayerTitleRow

--- androidApp/src/main/java/com/mutualmobile/iswipe/android/view/screens/youtube_screen/components/ExpandedPlayerTitleRow.kt
+++ androidApp/src/main/java/com/mutualmobile/iswipe/android/view/screens/youtube_screen/components/ExpandedPlayerTitleRow.kt
@@ -18,7 +18,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mutualmobile.iswipe.android.view.theme.YoutubePlayerTypography
 import com.mutualmobile.iswipe.android.viewmodels.YoutubeViewModel
 import org.koin.androidx.compose.get
@@ -53,19 +57,36 @@ fun ExpandedPlayerTitleRow(youtubeViewModel: YoutubeViewModel = get()) {
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                "${currentVideoItem?.statistics?.viewCount} views ",
-                style = YoutubePlayerTypography.bodySmall,
-                color = MaterialTheme.colorScheme.onSurface.copy(0.75f),
-                modifier = Modifier.padding(top = ExpandedPlayerTitleRow.TEXT_PADDING_TOP.dp)
+            ExpandedPlayerTitleRowText(
+                texts = arrayOf(
+                    Pair("${currentVideoItem?.statistics?.viewCount} views ", false),
+                    Pair("${currentVideoItem?.snippet?.publishedAt} ago", false),
+                    Pair(
+                        currentVideoItem?.snippet?.tags?.subList(0, 3)?.joinToString(transform = { existingText ->
+                            "#$existingText"
+                        }, separator = " ").orEmpty(),
+                        true
+                    )
+                )
             )
             YoutubeCaptionDotDivider(paddingTop = ExpandedPlayerTitleRow.TEXT_PADDING_TOP.dp)
-            Text(
-                " ${currentVideoItem?.snippet?.publishedAt} ago",
-                style = YoutubePlayerTypography.bodySmall,
-                color = MaterialTheme.colorScheme.onSurface.copy(0.75f),
-                modifier = Modifier.padding(top = ExpandedPlayerTitleRow.TEXT_PADDING_TOP.dp)
-            )
         }
     }
+}
+
+@Composable
+private fun ExpandedPlayerTitleRowText(
+    vararg texts: Pair<String, Boolean>
+) {
+    val builder = buildAnnotatedString {
+        texts.forEach { textPair ->
+            val textColor = if (textPair.second) MaterialTheme.colorScheme.tertiary else MaterialTheme.colorScheme.onSurface.copy(0.75f)
+            append(AnnotatedString("${textPair.first} ", spanStyle = SpanStyle(color = textColor, fontSize = 12.sp)))
+        }
+    }
+
+    Text(
+        builder,
+        modifier = Modifier.padding(top = ExpandedPlayerTitleRow.TEXT_PADDING_TOP.dp)
+    )
 }
